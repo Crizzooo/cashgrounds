ce145dc6e63a5f1adce50e5a02d944ea167afae9
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-06-04 15:14:16 -0400

    feat(added procFile and process.env.PORT for deployment)

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 const app = express();
