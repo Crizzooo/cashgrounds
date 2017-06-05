a7c6e7cb28f71a844a9a25e28ec68d37e53ff223
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-06-05 14:18:17 -0400

    fix(create babelrc file so mocha can compile correctly)

---
+++ tests/index.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+
+describe('fakeTests', () => {
+
+      it('should pass fake tests', () => {
+        expect(1).to.be(1);
+        expect('hello').to.be('hello');
+      });
+
+})
