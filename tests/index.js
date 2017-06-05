7531d0f25f87f904e5e459d6dd333e65fc928405
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-06-05 14:32:46 -0400

    feat(fix tests use of to so that fake tests pass CI Build)

--- tests/index.js
+++ tests/index.js
@@ -3,8 +3,8 @@ import {expect} from 'chai';
 describe('fakeTests', () => {
 
       it('should pass fake tests', () => {
-        expect(1).to.be(1);
-        expect('hello').to.be('hello');
+        expect(1).to.equal(1);
+        expect('hello').to.equal('hello');
       });
 
 })
