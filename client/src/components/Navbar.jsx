69df7e9c550e07e7cd86e30000970d61648e9ba9
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-06-13 17:23:19 -0400

    feat(added padding on navbar)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ class Navbar extends Component {
           <AppBar
             title={(<span className="navLogoContainer"><img  src="/assets/cashgroundsTakeFour.png" className="navLogo"></img></span>)}
             titleStyle={{"text-align":"center", "margin-left":"-40px", "color":`${Styles.main_accent}`}}
-            style={{"background-color":`rgba(0, 0, 0, 0.2)`}}
+            style={{"background-color":`rgba(0, 0, 0, 0.2)`,"padding-top":"10px","padding-bottom":"10px"}}
             />
       </div>
     );
