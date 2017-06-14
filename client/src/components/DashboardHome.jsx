5e103af1191a62f636ba22f9aaf54ec4726c674e
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-06-13 22:37:21 -0400

    fix(lowered dashboard demo from 12 caerds to 8)

--- client/src/components/DashboardHome.jsx
+++ client/src/components/DashboardHome.jsx
@@ -19,7 +19,7 @@ class DashboardHome extends Component {
     console.log('in dashboard');
     return (
         <div className="row center-xs around-xs">
-          { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map( elem => <DashboardCard />) }
+          { [1, 2, 3, 4, 5, 6, 7, 8].map( elem => <DashboardCard />) }
         </div>
     );
   }
