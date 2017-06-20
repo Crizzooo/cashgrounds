b55e0571711b50cba64d852f5cfad487f094a743
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-06-20 11:24:22 -0400

    fix (Sequelize connect now uses dialectOptions: ssl true to solve pg error)

--- server/db/index.js
+++ server/db/index.js
@@ -2,4 +2,10 @@ const Sequelize = require('sequelize');
 
 const url = process.env.DATABASE_URL || 'postgres://localhost:5432/cashgrounds';
 
-module.exports = new Sequelize(url, {});
+module.exports = new Sequelize(url, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
